67899db6b0f019cd68b4158c483b33fcbf8a1399
kenichi0624 <54873006+kenichi0624@users.noreply.github.com>
2019-12-01 14:05:58 -0600

    changes
    changes in calories

--- CalorieIntake/Form1.Designer.cs
+++ CalorieIntake/Form1.Designer.cs
@@ -50,124 +50,132 @@
             // 
             this.Title.AutoSize = true;
             this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(523, 24);
+            this.Title.Location = new System.Drawing.Point(479, 21);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(294, 42);
+            this.Title.Size = new System.Drawing.Size(197, 29);
             this.Title.TabIndex = 0;
             this.Title.Text = "Calorie Counter";
             // 
             // ItemName
             // 
             this.ItemName.AutoSize = true;
-            this.ItemName.Location = new System.Drawing.Point(162, 177);
+            this.ItemName.Location = new System.Drawing.Point(90, 85);
             this.ItemName.Name = "ItemName";
-            this.ItemName.Size = new System.Drawing.Size(166, 33);
+            this.ItemName.Size = new System.Drawing.Size(106, 24);
             this.ItemName.TabIndex = 1;
             this.ItemName.Text = "Item Name:";
             // 
             // Fat
             // 
             this.Fat.AutoSize = true;
-            this.Fat.Location = new System.Drawing.Point(501, 249);
+            this.Fat.Location = new System.Drawing.Point(333, 144);
             this.Fat.Name = "Fat";
-            this.Fat.Size = new System.Drawing.Size(138, 33);
+            this.Fat.Size = new System.Drawing.Size(87, 24);
             this.Fat.TabIndex = 2;
             this.Fat.Text = "Total Fat:";
             // 
             // Carbs
             // 
             this.Carbs.AutoSize = true;
-            this.Carbs.Location = new System.Drawing.Point(352, 314);
+            this.Carbs.Location = new System.Drawing.Point(237, 209);
             this.Carbs.Name = "Carbs";
-            this.Carbs.Size = new System.Drawing.Size(287, 33);
+            this.Carbs.Size = new System.Drawing.Size(183, 24);
             this.Carbs.TabIndex = 3;
             this.Carbs.Text = "Total Carbohydrates:";
             // 
             // Protein
             // 
             this.Protein.AutoSize = true;
-            this.Protein.Location = new System.Drawing.Point(524, 374);
+            this.Protein.Location = new System.Drawing.Point(346, 271);
             this.Protein.Name = "Protein";
-            this.Protein.Size = new System.Drawing.Size(115, 33);
+            this.Protein.Size = new System.Drawing.Size(74, 24);
             this.Protein.TabIndex = 4;
             this.Protein.Text = "Protein:";
             // 
             // Fiber
             // 
             this.Fiber.AutoSize = true;
-            this.Fiber.Location = new System.Drawing.Point(549, 432);
+            this.Fiber.Location = new System.Drawing.Point(226, 330);
             this.Fiber.Name = "Fiber";
-            this.Fiber.Size = new System.Drawing.Size(90, 33);
+            this.Fiber.Size = new System.Drawing.Size(195, 24);
             this.Fiber.TabIndex = 5;
-            this.Fiber.Text = "Fiber:";
+            this.Fiber.Text = "Serving per container:";
+            this.Fiber.Click += new System.EventHandler(this.Fiber_Click);
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(334, 174);
+            this.nameInput.Location = new System.Drawing.Point(223, 85);
             this.nameInput.Name = "nameInput";
-            this.nameInput.Size = new System.Drawing.Size(544, 40);
+            this.nameInput.Size = new System.Drawing.Size(198, 28);
             this.nameInput.TabIndex = 6;
+            this.nameInput.TextChanged += new System.EventHandler(this.nameInput_TextChanged);
+            this.nameInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nameInput_KeyPress);
             // 
             // carbInput
             // 
-            this.carbInput.Location = new System.Drawing.Point(664, 311);
+            this.carbInput.Location = new System.Drawing.Point(462, 205);
             this.carbInput.Name = "carbInput";
-            this.carbInput.Size = new System.Drawing.Size(214, 40);
+            this.carbInput.Size = new System.Drawing.Size(214, 28);
             this.carbInput.TabIndex = 7;
+            this.carbInput.TextChanged += new System.EventHandler(this.carbInput_TextChanged);
             // 
             // proteinInput
             // 
-            this.proteinInput.Location = new System.Drawing.Point(664, 374);
+            this.proteinInput.Location = new System.Drawing.Point(462, 268);
             this.proteinInput.Name = "proteinInput";
-            this.proteinInput.Size = new System.Drawing.Size(214, 40);
+            this.proteinInput.Size = new System.Drawing.Size(214, 28);
             this.proteinInput.TabIndex = 8;
+            this.proteinInput.TextChanged += new System.EventHandler(this.proteinInput_TextChanged);
             // 
             // fiberInput
             // 
-            this.fiberInput.Location = new System.Drawing.Point(664, 432);
+            this.fiberInput.Location = new System.Drawing.Point(462, 326);
             this.fiberInput.Name = "fiberInput";
-            this.fiberInput.Size = new System.Drawing.Size(214, 40);
+            this.fiberInput.Size = new System.Drawing.Size(214, 28);
             this.fiberInput.TabIndex = 9;
+            this.fiberInput.TextChanged += new System.EventHandler(this.fiberInput_TextChanged);
             // 
             // fatInput
             // 
-            this.fatInput.Location = new System.Drawing.Point(664, 246);
+            this.fatInput.Location = new System.Drawing.Point(462, 140);
             this.fatInput.Name = "fatInput";
-            this.fatInput.Size = new System.Drawing.Size(214, 40);
+            this.fatInput.Size = new System.Drawing.Size(214, 28);
             this.fatInput.TabIndex = 10;
+            this.fatInput.TextChanged += new System.EventHandler(this.fatInput_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(715, 102);
+            this.label1.Location = new System.Drawing.Point(687, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 33);
+            this.label1.Size = new System.Drawing.Size(53, 24);
             this.label1.TabIndex = 11;
             this.label1.Text = "Date:";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(829, 96);
+            this.dateTimePicker1.Location = new System.Drawing.Point(746, 88);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(504, 40);
+            this.dateTimePicker1.Size = new System.Drawing.Size(334, 28);
             this.dateTimePicker1.TabIndex = 12;
             // 
             // dataEntry
             // 
             this.dataEntry.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.dataEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dataEntry.Location = new System.Drawing.Point(676, 512);
+            this.dataEntry.Location = new System.Drawing.Point(484, 397);
             this.dataEntry.Name = "dataEntry";
             this.dataEntry.Size = new System.Drawing.Size(183, 65);
             this.dataEntry.TabIndex = 13;
             this.dataEntry.Text = "Enter Data";
             this.dataEntry.UseVisualStyleBackColor = false;
+            this.dataEntry.Click += new System.EventHandler(this.dataEntry_Click);
             // 
             // Close
             // 
             this.Close.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Close.Location = new System.Drawing.Point(969, 600);
+            this.Close.Location = new System.Drawing.Point(732, 397);
             this.Close.Name = "Close";
             this.Close.Size = new System.Drawing.Size(183, 65);
             this.Close.TabIndex = 14;
@@ -179,7 +187,7 @@
             // 
             this.clear.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clear.Location = new System.Drawing.Point(389, 600);
+            this.clear.Location = new System.Drawing.Point(223, 397);
             this.clear.Name = "clear";
             this.clear.Size = new System.Drawing.Size(183, 65);
             this.clear.TabIndex = 15;
@@ -189,10 +197,10 @@
             // 
             // CalorieIntake
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SkyBlue;
-            this.ClientSize = new System.Drawing.Size(1385, 693);
+            this.ClientSize = new System.Drawing.Size(1137, 500);
             this.Controls.Add(this.clear);
             this.Controls.Add(this.Close);
             this.Controls.Add(this.dataEntry);
@@ -210,7 +218,7 @@
             this.Controls.Add(this.ItemName);
             this.Controls.Add(this.Title);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CalorieIntake";
             this.Text = "Calorie Intake";
             this.ResumeLayout(false);
--- CalorieIntake/Form1.cs
+++ CalorieIntake/Form1.cs
@@ -29,6 +29,172 @@ namespace CalorieIntake
             fatInput.Clear();
             proteinInput.Clear();
             fiberInput.Clear();
+
+            dateTimePicker1.ResetText();
+        }
+
+        private void nameInput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void fatInput_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (System.Text.RegularExpressions.Regex.IsMatch(fatInput.Text, "[^0-9]"))
+            {
+                fatInput.Clear();
+
+                MessageBox.Show("Please enter only numbers.");
+                return;
+
+            }
+        }
+
+        private void carbInput_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(carbInput.Text, "[^0-9]"))
+            {
+                carbInput.Clear();
+
+                MessageBox.Show("Please enter only numbers.");
+                return;
+
+            }
+        }
+
+        private void proteinInput_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(proteinInput.Text, "[^0-9]"))
+            {
+                proteinInput.Clear();
+
+                MessageBox.Show("Please enter only numbers.");
+                return;
+
+            }
+        }
+
+        private void Fiber_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fiberInput_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(fiberInput.Text, "[^0-9]"))
+            {
+                fiberInput.Clear();
+
+                MessageBox.Show("Please enter only numbers.");
+                return;
+
+            }
+        }
+
+        private void dataEntry_Click(object sender, EventArgs e)
+        {
+            double fatGrams, carbGrams, proteinGrams, fiberGrams;
+            string ItemName;
+            string theDate = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+
+
+            fatGrams = double.Parse(fatInput.Text);
+            carbGrams = double.Parse(carbInput.Text);
+            proteinGrams = double.Parse(proteinInput.Text);
+            fiberGrams = double.Parse(fiberInput.Text);
+            ItemName = (nameInput.Text).ToString();
+
+
+            if (fatGrams > 0)
+            {
+                fatGrams = fatGrams * 9;
+            }
+
+            else
+            {
+                string message1 = "Please enter correct value of Fat";
+                string title1 = "Hint";
+                MessageBox.Show(message1, title1);
+                return;
+            }
+
+            if (carbGrams > 0)
+            {
+                carbGrams = carbGrams * 4;
+            }
+            else
+            {
+                string message2 = "Please enter correct value of Carbohydrates";
+                string title2 = "Hint";
+                MessageBox.Show(message2, title2);
+                return;
+            }
+
+            if (proteinGrams > 0)
+            {
+                proteinGrams = proteinGrams * 4;
+            }
+            else
+            {
+                string message3 = "Please enter correct value of Protein";
+                string title3 = "Hint";
+                MessageBox.Show(message3, title3);
+                return;
+            }
+
+            if (proteinGrams > 0)
+            {
+                fiberGrams = (fatGrams + carbGrams + proteinGrams) * fiberGrams;
+            }
+            else
+            {
+                string message4 = "Please enter correct value of serving amount of container";
+                string title4 = "Hint";
+                MessageBox.Show(message4, title4);
+                return;
+            }
+
+            if (ItemName == "")
+            {
+                string message5 = "Please enter correct name";
+                string title5 = "Hint";
+                MessageBox.Show(message5, title5);
+                return;
+            }
+
+
+            string message = ItemName + " Has Calories of : " + fiberGrams;
+            string title = theDate;
+            MessageBox.Show(message, title);
+
+
+        }
+
+        private void fatCalories_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(fatInput.Text, "[^0-9]"))
+            {
+                fatInput.Clear();
+
+                MessageBox.Show("Please enter only numbers.");
+                return;
+
+            }
+        }
+
+        private void nameInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsLetter(e.KeyChar))
+                {
+                    e.Handled = true;
+
+                    MessageBox.Show("Please enter only Item Name.");
+                    return;
+                }
+            }
         }
     }
-}
+ }
+
